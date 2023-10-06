9a7ba9fa88ad206d42c160f2d8a37ff1ef2734de
evportel <evportel@student.42sp.org.br>
2023-10-05 22:36:16 -0300

    input's = conversao para inteiros - OK

--- ft_receive_entries.c
+++ ft_receive_entries.c
@@ -1,34 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_receive_entries.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/05 21:48:22 by evportel          #+#    #+#             */
-/*   Updated: 2023/10/05 22:31:23 by evportel         ###   ########.fr       */
+/*   Created: 2023/10/05 21:44:55 by evportel          #+#    #+#             */
+/*   Updated: 2023/10/05 22:30:12 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+int	*ft_receive_entries(int argc, char **argv)
 {
+	int	*entries;
 	int	index;
-	int *stack;
 
-	if (argc < 2)
+	entries = (int *) malloc((argc - 1) * sizeof(int));
+	if (entries == 0)
+		return NULL;
+	index = 0;
+	while (index < argc -1)
 	{
-		ft_printf("Error: Invalid arguments\n");
-		return (EXIT_FAILURE);
+		entries[index] = ft_atoi(&argv[index + 1][0]);
+		index++;
 	}
-	else
-	{
-		stack = ft_receive_entries(argc, argv);
-		index = 0;
-		while (index < (argc - 1))
-			ft_printf("é número: %d\n", stack[index++]);
-		
-	}
-	return (EXIT_SUCCESS);
+	return (entries);
 }
