b70d91fbd235884574f6e0cb29d354de6eb97b3e
evportel <evportel@student.42sp.org.br>
2023-10-12 12:20:54 -0300

    feat: checagem de numeros duplicados nos parametros

--- sources/ft_receive_inputs.c
+++ sources/ft_receive_inputs.c
@@ -6,11 +6,31 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:16:52 by evportel          #+#    #+#             */
-/*   Updated: 2023/10/12 11:26:00 by evportel         ###   ########.fr       */
+/*   Updated: 2023/10/12 12:19:28 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static int	ft_check_duplicates(int argc, int *array_numbers)
+{
+	int	index;
+	int	suspect;
+
+	index = 0;
+	while (index < argc)
+	{
+		suspect = index + 1;
+		while (suspect < argc)
+		{
+			if (array_numbers[index] == array_numbers[suspect])
+				return (EXIT_FAILURE);
+			suspect++;
+		}
+		index++;
+	}
+	return (EXIT_SUCCESS);
+}
 
 void	ft_receive_inputs(int argc, char **argv, int *array_numbers)
 {
@@ -22,4 +42,6 @@ void	ft_receive_inputs(int argc, char **argv, int *array_numbers)
 		array_numbers[index] = ft_atoi(argv[index]);
 		index++;
 	}
+	if (ft_check_duplicates(argc, array_numbers) == EXIT_FAILURE) 
+		ft_push_swap_error();
 }
