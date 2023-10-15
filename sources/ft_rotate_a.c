a5ad7cf02ae6865cda3277daccf6f12ab13315cd
evportel <evportel@student.42sp.org.br>
2023-10-15 18:08:47 -0300

    feat: rotate_a e rotate_b

---
+++ sources/ft_rotate_a.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rotate_a.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 17:35:14 by evportel          #+#    #+#             */
+/*   Updated: 2023/10/15 17:47:41 by evportel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_rotate_a(t_stack_pack *pack)
+{
+	t_stack	*first;
+	t_stack	*last;
+
+	if (pack->stack_a != NULL)
+	{
+		first = pack->stack_a;
+		last = pack->stack_a;
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		pack->stack_a = first->next;
+		first->next = NULL;
+		last->next = first;
+		ft_putstr_fd("ra\n", 1);
+	}
+}
---
+++ sources/ft_rotate_b.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rotate_b.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 17:35:14 by evportel          #+#    #+#             */
+/*   Updated: 2023/10/15 18:08:02 by evportel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_rotate_b(t_stack_pack *pack)
+{
+	t_stack	*first;
+	t_stack	*last;
+
+	if (pack->stack_b != NULL)
+	{
+		first = pack->stack_b;
+		last = pack->stack_b;
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		pack->stack_b = first->next;
+		first->next = NULL;
+		last->next = first;
+		ft_putstr_fd("rb\n", 1);
+	}
+}
