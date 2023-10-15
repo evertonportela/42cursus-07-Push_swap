0e8b3b375fd4e3d339dbd856e368c7579f73b4ed
evportel <evportel@student.42sp.org.br>
2023-10-15 18:09:17 -0300

    feat: operation push_a e push_b

---
+++ sources/ft_push_a.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push_a.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 17:52:35 by evportel          #+#    #+#             */
+/*   Updated: 2023/10/15 18:01:13 by evportel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_push_a(t_stack_pack *pack)
+{
+	t_stack *first;
+
+	if (pack->stack_b != NULL)
+	{
+		first = pack->stack_b;
+		pack->stack_b = first->next;
+		first->next = pack->stack_a;
+		pack->stack_a = first;
+		ft_putstr_fd("pa\n", 1);
+	}
+}
---
+++ sources/ft_push_b.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push_b.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 17:52:35 by evportel          #+#    #+#             */
+/*   Updated: 2023/10/15 17:59:27 by evportel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_push_b(t_stack_pack *pack)
+{
+	t_stack *first;
+
+	if (pack->stack_a != NULL)
+	{
+		first = pack->stack_a;
+		pack->stack_a = first->next;
+		first->next = pack->stack_b;
+		pack->stack_b = first;
+		ft_putstr_fd("pb\n", 1);
+	}
+}
