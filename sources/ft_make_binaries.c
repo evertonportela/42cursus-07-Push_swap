eae3212aceb53b7d989ed55c7106c655c4269094
Everton P. <evertonportela@outlook.com>
2023-10-23 14:09:27 +0000

    comment: docs funcoes push long

--- sources/ft_make_binaries.c
+++ sources/ft_make_binaries.c
@@ -13,43 +13,48 @@
 #include "push_swap.h"
 
 /**
- * Recebe um valor 'length' e calcula o número de vezes que 'length' 
- * pode ser dividido por '2', antes de se tornar zero.
- * Será usado para determinar o tamanho da alocação de memória necessária.
-*/
+ * Calcula o número de vezes que 'length' pode ser dividido por 2 antes de se tornar zero.
+ *
+ * @param length O número a ser dividido.
+ * @return O número de vezes que 'length' pode ser dividido por 2.
+ */
 int	ft_find_limit(int length)
 {
-	int	limit;
+	int	limit;		// O número de vezes que 'length' pode ser dividido por 2.
 
 	limit = 0;
 	while ((length / 2) != 0)
 	{
+        // Enquanto o valor de 'length' dividido por 2 não for zero, continue o loop.
 		limit++;
 		length = length / 2;
 	}
+	// Retorna o número de vezes que 'length' pode ser dividido por 2.
 	return (limit);
 }
 
 /**
- * Converte o 'number' recebido para a representação em binário 
- * e armazena numa string.
- * Calcula o limite de armzenamento e +1 para o '\0'
- * Faz uma operação de deslocamento à direita e extrai o bit mais significativo
- * Observe que esse mais significativo faz muita diferenca aqui.
- * Converte o valor usando referencial da tabela ASCII
- * e retonar a representação binária.
-*/
+ * Converte um número inteiro em sua representação binária e retorna como uma string.
+ *
+ * @param length O número de dígitos necessários para representar o número.
+ * @param number O número inteiro a ser convertido.
+ * @return Uma string contendo a representação binária do número.
+ */
 static char	*ft_itob(int length, int number)
 {
-	int		limit;
-	int		bit_value;
-	int		bit_position;
-	int		string_index;
-	char	*binary_string;
+	int		limit;				// O número de dígitos necessários.
+	int		bit_value;			// O valor do dígito binário atual (0 ou 1).
+	int		bit_position;		// A posição do dígito binário atual.
+	int		string_index;		// O índice da string binária.
+	char	*binary_string;		// A string binária resultante.
 
+    // Calcula o número de dígitos necessários para representar o número em binário (encontrando o limite de armazenamento).
 	limit = ft_find_limit(length);
 	
+    // Aloca memória para a string binária com base no número de dígitos calculados.
 	binary_string = (char *)malloc(limit * sizeof(char *) + 1);
+
+    // Em caso de falha na alocação, encerra o programa.
 	if (binary_string == NULL)
 		exit(EXIT_FAILURE);
 	
@@ -57,8 +62,10 @@ static char	*ft_itob(int length, int number)
 	string_index = 0;
 	while (bit_position >= 0)
 	{
+        // Desloca o número original para a direita (bit a bit) para extrair o dígito binário da posição atual.
 		bit_value = number >> bit_position;
 		
+        // Verifica se o dígito extraído é 0 ou 1 e o converte em caractere correspondente ('0' ou '1').
 		if (bit_value & 1)
 			*(binary_string + string_index) = 1 + '0';
 		else
@@ -67,28 +74,39 @@ static char	*ft_itob(int length, int number)
 		string_index++;
 		bit_position--;
 	}
-
+    // Adiciona o caractere nulo '\0' para indicar o fim da string.
 	*(binary_string + string_index) = '\0';
 	
+	// Retorna a string binária como resultado.	
 	return (binary_string);
 }
 
+/**
+ * Cria um array de strings contendo a representação binária de números inteiros.
+ *
+ * @param length O tamanho do array de números inteiros.
+ * @param numbers O array de números inteiros.
+ * @return Um novo array de strings contendo as representações binárias dos números.
+ */
 char	**ft_make_binaries(int length, int *numbers)
 {
-	int		index;
-	char	**array_bin;
+	int		index;			// Índice usado para percorrer o array de números inteiros.
+	char	**array_bin;	// Um novo array de strings para armazenar as representações binárias.
 
+    // Aloca memória para o novo array de strings com base no tamanho do array de números inteiros.
 	array_bin = (char **)malloc(length * (sizeof(char **)) + 1);
-	// array_bin = (char **)malloc(length * (sizeof(char **)));
+
+	// Em caso de falha na alocação, encerra o programa.
 	if (array_bin == NULL)
 		exit(EXIT_FAILURE);
 
 	index = 0;
 	while (index < length)
 	{
+        // Converte o número inteiro em sua representação binária e armazena no novo array.
 		array_bin[index] = ft_itob(length, numbers[index]);
-		ft_printf("%s\n", array_bin[index]);
 		index++;
 	}
+	// Retorna o novo array de strings como resultado.
 	return (array_bin);
 }
--- sources/ft_push_swap.c
+++ sources/ft_push_swap.c
@@ -13,44 +13,59 @@
 #include "push_swap.h"
 
 /**
- * Função interna para ordenar uma lista usando o algoritmo "push_swap" 
- * para listas longas, maiores que 5 itens.
-*/
+ * Função responsável por classificar pilhas longas (mais de 5 elementos) usando o algoritmo Push Swap.
+ *
+ * @param pack Uma estrutura que contém as pilhas e outras informações necessárias.
+ */
 static void	ft_push_swap_long(t_stack_pack *pack)
 {
 	int	count;
 
 	count = 0;
-	// Encontra o limite com base no tamanho da pilha
+
+	// Calcula o limite com base no tamanho da pilha
 	pack->limit = ft_find_limit(pack->length);
 	while (pack->limit >= 0)
 	{
 		while(count < pack->length)
+		// Verifica o bit no índice 'pack->limit' do elemento atual da pilha A
 		{
 			if (pack->stack_a->content[pack->limit] == '0')
 			{
-
+				// Empilha o elemento na pilha B
 				ft_push_b(pack);
 			}
 			else
 			{
+				// Rotaciona a pilha A
 				ft_rotate_a(pack);
 			}
 			count++;
 		}
 		while (pack->stack_b)
+		{
+			// Desempilha elementos da pilha B de volta para a pilha A
 			ft_push_a(pack);
+		}
+		// Reduz o limite para processar o próximo bit significativo
 		pack->limit--;
+		// Reinicia o contador para processar o próximo bit
 		count = 0;
 	}
 }
 
+/**
+ * Função principal que inicia o processo de classificação das pilhas.
+ *
+ * @param pack Uma estrutura que contém as pilhas e outras informações necessárias.
+ */
 void	ft_push_swap(t_stack_pack *pack)
 {
-	// int i = index[0];   //remover
-	// i = 0;				//remover
+	// Se o tamanho da pilha for menor ou igual a 5, use uma estratégia de classificação curta.
 	if (pack->length <= 5 )
 		ft_push_swap_short(pack);
+	
+	// Caso contrário, use uma estratégia de classificação longa.
 	else
 		ft_push_swap_long(pack);
 }
